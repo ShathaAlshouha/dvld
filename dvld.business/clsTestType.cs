44e3c63b25a3d2edf546679fc61bfc7170fcd88f
Shatha AlShouha <shathaalshouha2003@gmail.com>
2025-04-16 12:19:55 -0700

    create-clsTestType-busniss

---
+++ dvld.business/clsTestType.cs
@@ -0,0 +1,49 @@
+﻿using dvld.data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dvld.business
+{
+    public class clsTestType
+    {
+
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+        public enum enTestType { VisionTest = 1, WrittenTest = 2, StreetTest = 3 };
+
+        public clsTestType.enTestType ID { set; get; }
+        public string Title { set; get; }
+        public string Description { set; get; }
+        public float Fees { set; get; }
+        public clsTestType()
+
+        {
+            this.ID = clsTestType.enTestType.VisionTest;
+            this.Title = "";
+            this.Description = "";
+            this.Fees = 0;
+            Mode = enMode.AddNew;
+
+        }
+
+        public clsTestType(clsTestType.enTestType ID, string TestTypeTitel, string Description, float TestTypeFees)
+
+        {
+            this.ID = ID;
+            this.Title = TestTypeTitel;
+            this.Description = Description;
+
+            this.Fees = TestTypeFees;
+            Mode = enMode.Update;
+        }
+
+      
+
+
+    }
+}
