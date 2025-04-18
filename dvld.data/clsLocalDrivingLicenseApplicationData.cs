2bd301d9c8529a271f28a5a6a625cc195562f634
Shatha AlShouha <shathaalshouha2003@gmail.com>
2025-04-18 02:00:21 -0700

    update-LocalDriving-Data

--- dvld.data/clsLocalDrivingLicenseApplicationData.cs
+++ dvld.data/clsLocalDrivingLicenseApplicationData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -56,6 +57,53 @@ namespace dvld.data
 
             return isFound;
         }
+
+
+
+        public static DataTable GetAllLocalDrivingLicenseApplications()
+        {
+
+            DataTable dt = new DataTable();
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"SELECT *
+                              FROM LocalDrivingLicenseApplications_View
+                              order by ApplicationDate Desc";
+
+
+
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+
+                {
+                    dt.Load(reader);
+                }
+
+                reader.Close();
+
+            }
+
+            catch (Exception ex)
+            {
+                // Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return dt;
+
+        }
+
 
     }
 }
