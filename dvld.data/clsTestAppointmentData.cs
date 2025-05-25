544ae1b0aa93986d53fc40c6f2fd657fa271815e
Shatha AlShouha <shathaalshouha2003@gmail.com>
2025-05-25 08:58:10 -0700

    create-testAppointments

---
+++ dvld.data/clsTestAppointmentData.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dvld.data
+{
+    public class clsTestAppointmentData
+    {
+        public static bool GetTestAppointmentInfoByID(int TestAppointmentID,
+     ref int TestTypeID, ref int LocalDrivingLicenseApplicationID,
+     ref DateTime AppointmentDate, ref float PaidFees, ref int CreatedByUserID, ref bool IsLocked, ref int RetakeTestApplicationID)
+        {
+            bool isFound = false;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "SELECT * FROM TestAppointments WHERE TestAppointmentID = @TestAppointmentID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@TestAppointmentID", TestAppointmentID);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+
+                    // The record was found
+                    isFound = true;
+                    TestTypeID = (int)reader["TestTypeID"];
+                    LocalDrivingLicenseApplicationID = (int)reader["LocalDrivingLicenseApplicationID"];
+                    AppointmentDate = (DateTime)reader["AppointmentDate"];
+                    CreatedByUserID = (int)reader["CreatedByUserID"];
+                    PaidFees = Convert.ToSingle(reader["PaidFees"]);
+                    IsLocked = (bool)reader["IsLocked"];
+
+                    if (reader["RetakeTestApplicationID"] == DBNull.Value)
+                        RetakeTestApplicationID = -1;
+                    else
+                        RetakeTestApplicationID = (int)reader["RetakeTestApplicationID"];
+
+                }
+                else
+                {
+                    // The record was not found
+                    isFound = false;
+                }
+
+                reader.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+                isFound = false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return isFound;
+        }
+
+    }
+
+    public static bool GetLastTestAppointment(
+         int LocalDrivingLicenseApplicationID, int TestTypeID,
+        ref int TestAppointmentID, ref DateTime AppointmentDate,
+        ref float PaidFees, ref int CreatedByUserID, ref bool IsLocked, ref int RetakeTestApplicationID)
+    {
+        bool isFound = false;
+
+        SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+        string query = @"SELECT       top 1 *
+                FROM            TestAppointments
+                WHERE        (TestTypeID = @TestTypeID) 
+                AND (LocalDrivingLicenseApplicationID = @LocalDrivingLicenseApplicationID) 
+                order by TestAppointmentID Desc";
+
+
+        SqlCommand command = new SqlCommand(query, connection);
+
+        command.Parameters.AddWithValue("@LocalDrivingLicenseApplicationID", LocalDrivingLicenseApplicationID);
+        command.Parameters.AddWithValue("@TestTypeID", TestTypeID);
+
+        try
+        {
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+
+                // The record was found
+                isFound = true;
+
+                TestAppointmentID = (int)reader["TestAppointmentID"];
+                AppointmentDate = (DateTime)reader["AppointmentDate"];
+                PaidFees = Convert.ToSingle(reader["PaidFees"]);
+                CreatedByUserID = (int)reader["CreatedByUserID"];
+                IsLocked = (bool)reader["IsLocked"];
+
+                if (reader["RetakeTestApplicationID"] == DBNull.Value)
+                    RetakeTestApplicationID = -1;
+                else
+                    RetakeTestApplicationID = (int)reader["RetakeTestApplicationID"];
+
+
+            }
+            else
+            {
+                // The record was not found
+                isFound = false;
+            }
+
+            reader.Close();
+
+
+        }
+        catch (Exception ex)
+        {
+            //Console.WriteLine("Error: " + ex.Message);
+            isFound = false;
+        }
+        finally
+        {
+            connection.Close();
+        }
+
+        return isFound;
+    }
+}
