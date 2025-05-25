14ce8089ff8ab39a9c674ec9751be76c2414502c
Shatha AlShouha <shathaalshouha2003@gmail.com>
2025-05-25 08:32:55 -0700

    update clsLocaalDrivingLisenceApplication

--- dvld.business/clsLocalDrivingLicenseApplication.cs
+++ dvld.business/clsLocalDrivingLicenseApplication.cs
@@ -1,6 +1,7 @@
 ﻿using dvld.data;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,7 @@ namespace dvld.business
         {
             get
             {
-                return clsPerson.Find(ApplicantPersonID).FullName;
+                return base.PersonInfo.FullName;
             }
 
         }
@@ -103,6 +104,122 @@ namespace dvld.business
 
         }
 
+
+        public static clsLocalDrivingLicenseApplication FindByApplicationID(int ApplicationID)
+        {
+          
+            int LocalDrivingLicenseApplicationID = -1, LicenseClassID = -1;
+
+            bool IsFound = clsLocalDrivingLicenseApplicationData.GetLocalDrivingLicenseApplicationInfoByApplicationID
+                (ApplicationID, ref LocalDrivingLicenseApplicationID, ref LicenseClassID);
+
+
+            if (IsFound)
+            {
+              
+                clsApplication Application = clsApplication.FindBaseApplication(ApplicationID);
+
+                return new clsLocalDrivingLicenseApplication(
+                    LocalDrivingLicenseApplicationID, Application.ApplicationID,
+                    Application.ApplicantPersonID,
+                                     Application.ApplicationDate, Application.ApplicationTypeID,
+                                    (enApplicationStatus)Application.ApplicationStatus, Application.LastStatusDate,
+                                     Application.PaidFees, Application.CreatedByUserID, LicenseClassID);
+            }
+            else
+                return null;
+
+
+        }
+
+        public bool Save()
+        {
+
+            //Because of inheritance first we call the save method in the base class,
+            //it will take care of adding all information to the application table.
+            base.Mode = (clsApplication.enMode)Mode;
+            if (!base.Save())
+                return false;
+
+
+            //After we save the main application now we save the sub application.
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewLocalDrivingLicenseApplication())
+                    {
+
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+
+                    return _UpdateLocalDrivingLicenseApplication();
+
+            }
+
+            return false;
+        }
+
+        public static DataTable GetAllLocalDrivingLicenseApplications()
+        {
+            return clsLocalDrivingLicenseApplicationData.GetAllLocalDrivingLicenseApplications();
+        }
+
+        public bool Delete()
+        {
+            bool IsLocalDrivingApplicationDeleted = false;
+            bool IsBaseApplicationDeleted = false;
+            //First we delete the Local Driving License Application
+            IsLocalDrivingApplicationDeleted = clsLocalDrivingLicenseApplicationData.DeleteLocalDrivingLicenseApplication(this.LocalDrivingLicenseApplicationID);
+
+            if (!IsLocalDrivingApplicationDeleted)
+                return false;
+            //Then we delete the base Application
+            IsBaseApplicationDeleted = base.Delete();
+            return IsBaseApplicationDeleted;
+
+        }
+
+        public bool DoesPassTestType(clsTestType.enTestType TestTypeID)
+
+        {
+            return clsLocalDrivingLicenseApplicationData.DoesPassTestType(this.LocalDrivingLicenseApplicationID, (int)TestTypeID);
+        }
+        public bool DoesPassPreviousTest(clsTestType.enTestType CurrentTestType)
+        {
+
+            switch (CurrentTestType)
+            {
+                case clsTestType.enTestType.VisionTest:
+                    //in this case no required prvious test to pass.
+                    return true;
+
+                case clsTestType.enTestType.WrittenTest:
+                    //Written Test, you cannot sechdule it before person passes the vision test.
+                    //we check if pass visiontest 1.
+
+                    return this.DoesPassTestType(clsTestType.enTestType.VisionTest);
+
+
+                case clsTestType.enTestType.StreetTest:
+
+                    //Street Test, you cannot sechdule it before person passes the written test.
+                    //we check if pass Written 2.
+                    return this.DoesPassTestType(clsTestType.enTestType.WrittenTest);
+
+                default:
+                    return false;
+            }
+        }
+
+
+ 
 
     }
 }
