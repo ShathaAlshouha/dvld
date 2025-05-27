36ffed8dea5599e78e5bd551e2c69552b636b93c
Shatha AlShouha <shathaalshouha2003@gmail.com>
2025-05-27 07:22:58 -0700

    create clsTestAppointment

---
+++ dvld.business/clsTestAppointment.cs
@@ -0,0 +1,67 @@
+﻿using dvld.data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dvld.business
+{
+    public class clsTestAppointment
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int TestAppointmentID { set; get; }
+        public clsTestType.enTestType TestTypeID { set; get; }
+     
+        public int LocalDrivingLicenseApplicationID { set; get; }
+        public DateTime AppointmentDate { set; get; }
+        public float PaidFees { set; get; }
+        public int CreatedByUserID { set; get; }
+        public bool IsLocked { set; get; }
+        public int RetakeTestApplicationID { set; get; }
+        public clsApplication RetakeTestAppInfo { set; get; }
+
+        public clsTestAppointment()
+        {
+            this.TestAppointmentID = 0;
+            this.TestTypeID = clsTestType.enTestType.VisionTest;
+            this.AppointmentDate = DateTime.Now;
+            this.PaidFees = 0;
+            this.LocalDrivingLicenseApplicationID = 0;
+            this.CreatedByUserID = 0;
+            this.IsLocked = false;
+            this.RetakeTestApplicationID = 0;
+            Mode = enMode.AddNew;
+
+        }
+        public clsTestAppointment(int TestAppointmentID, clsTestType.enTestType TestTypeID,
+           int LocalDrivingLicenseApplicationID, DateTime AppointmentDate, float PaidFees,
+           int CreatedByUserID, bool IsLocked, int RetakeTestApplicationID)
+        {
+
+            this.TestAppointmentID = TestAppointmentID;
+            this.TestTypeID = TestTypeID;
+            this.LocalDrivingLicenseApplicationID = LocalDrivingLicenseApplicationID;
+            this.AppointmentDate = AppointmentDate;
+            this.PaidFees = PaidFees;
+            this.CreatedByUserID = CreatedByUserID;
+            this.IsLocked = IsLocked;
+            this.RetakeTestApplicationID = RetakeTestApplicationID;
+            this.RetakeTestAppInfo = clsApplication.FindBaseApplication(RetakeTestApplicationID);
+            Mode = enMode.Update;
+        }
+
+        private bool _AddNewTestAppointment()
+        {
+
+            this.TestAppointmentID = clsTestAppointmentData.AddNewTestAppointment((int)this.TestTypeID, this.LocalDrivingLicenseApplicationID,
+                this.AppointmentDate, this.PaidFees, this.CreatedByUserID, this.RetakeTestApplicationID);
+
+            return (this.TestAppointmentID != -1);
+        }
+
+    }
+}
