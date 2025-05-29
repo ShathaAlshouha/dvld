723ee50eba4a68b0c477441b5d5cb31621273f18
Shatha AlShouha <shathaalshouha2003@gmail.com>
2025-05-29 01:36:55 -0700

    update tests

--- dvld.data/clsTestData.cs
+++ dvld.data/clsTestData.cs
@@ -173,6 +173,106 @@ namespace dvld.data
 
         }
 
+        public static int AddNewTest(int TestAppointmentID, bool TestResult,
+            string Notes, int CreatedByUserID)
+        {
+            int TestID = -1;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"Insert Into Tests (TestAppointmentID,TestResult,
+                                                Notes,   CreatedByUserID)
+                            Values (@TestAppointmentID,@TestResult,
+                                                @Notes,   @CreatedByUserID);
+                            
+                                UPDATE TestAppointments 
+                                SET IsLocked=1 where TestAppointmentID = @TestAppointmentID;
+
+                                SELECT SCOPE_IDENTITY();";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@TestAppointmentID", TestAppointmentID);
+            command.Parameters.AddWithValue("@TestResult", TestResult);
+
+            if (Notes != "" && Notes != null)
+                command.Parameters.AddWithValue("@Notes", Notes);
+            else
+                command.Parameters.AddWithValue("@Notes", System.DBNull.Value);
+
+
+
+            command.Parameters.AddWithValue("@CreatedByUserID", CreatedByUserID);
+
+            try
+            {
+                connection.Open();
+
+                object result = command.ExecuteScalar();
+
+                if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                {
+                    TestID = insertedID;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+
+
+            return TestID;
+
+        }
+
+        public static bool UpdateTest(int TestID, int TestAppointmentID, bool TestResult,
+            string Notes, int CreatedByUserID)
+        {
+
+            int rowsAffected = 0;
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"Update  Tests  
+                            set TestAppointmentID = @TestAppointmentID,
+                                TestResult=@TestResult,
+                                Notes = @Notes,
+                                CreatedByUserID=@CreatedByUserID
+                                where TestID = @TestID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@TestID", TestID);
+            command.Parameters.AddWithValue("@TestAppointmentID", TestAppointmentID);
+            command.Parameters.AddWithValue("@TestResult", TestResult);
+            command.Parameters.AddWithValue("@Notes", Notes);
+            command.Parameters.AddWithValue("@CreatedByUserID", CreatedByUserID);
+
+            try
+            {
+                connection.Open();
+                rowsAffected = command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+                return false;
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+
+            return (rowsAffected > 0);
+        }
 
 
 
