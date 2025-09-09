925b6ba5bb0015a99557836bddcd885970c9b19e
Shatha AlShouha <shathaalshouha2003@gmail.com>
2025-09-09 15:06:10 +0300

    create userDTO

---
+++ DTOs/UserDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs
+{
+    public class UserDTO
+    {
+        public int UserID { set; get; }
+        public int PersonID { set; get; }
+        public string UserName { set; get; }
+        public string Password { set; get; }
+        public bool IsActive { set; get; }
+
+        public UserDTO()
+        {
+            UserID = -1;
+            PersonID = -1;
+            UserName = "";
+            Password = "";
+            IsActive = true;
+        }
+        public UserDTO(int userID, int personID, string userName, string password, bool isActive)
+        {
+            UserID = userID;
+            PersonID = personID;
+            UserName = userName;
+            Password = password;
+            IsActive = isActive;
+        }
+    }
+}
