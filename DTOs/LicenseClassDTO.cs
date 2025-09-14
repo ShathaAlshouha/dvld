2ae2190f7ad5d388f3090fe7a95637d5eed55f57
Shatha AlShouha <shathaalshouha2003@gmail.com>
2025-09-14 16:01:15 +0300

    create LicenseClassDTO

---
+++ DTOs/LicenseClassDTO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs
+{
+    public class LicenseClassDTO
+    {
+        public int LicenseClassID { set; get; }
+        public string ClassName { set; get; }
+
+        public string ClassDescription { set; get; }
+        public byte MinimumAllowedAge { set; get; }
+        public byte DefaultValidityLength { set; get; }
+
+        public float ClassFees { set; get; }
+        public LicenseClassDTO()
+        {
+            this.LicenseClassID = -1;
+            this.ClassName = "";
+            this.ClassDescription = "";
+            this.MinimumAllowedAge = 18;
+            this.DefaultValidityLength = 10;
+            this.ClassFees = 0;
+        }
+        public LicenseClassDTO(int LicenseClassID, string ClassName,
+            string ClassDescription,
+            byte MinimumAllowedAge, byte DefaultValidityLength, float ClassFees)
+        {
+            this.LicenseClassID = LicenseClassID;
+            this.ClassName = ClassName;
+            this.ClassDescription = ClassDescription;
+            this.MinimumAllowedAge = MinimumAllowedAge;
+            this.DefaultValidityLength = DefaultValidityLength;
+            this.ClassFees = ClassFees;
+        }
+
+         
+    }
+}
--- dvld.data/clsLicenseClassData.cs
+++ dvld.data/clsLicenseClassData.cs
@@ -265,5 +265,6 @@ namespace dvld.data
         }
 
 
+
     }
 }
