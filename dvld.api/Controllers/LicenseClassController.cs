488fa5a2af04e4f83165e0b962978481868dba89
Shatha AlShouha <shathaalshouha2003@gmail.com>
2025-09-15 14:55:12 +0300

    create LicenseClassController

--- dvld.api/Controllers/LicenseClassController.cs
+++ dvld.api/Controllers/LicenseClassController.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DTOs;
+using dvld.business;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dvld.api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+
     public class LicenseClassController : ControllerBase
     {
+        [HttpGet("All")]
+        public ActionResult<IEnumerable<LicenseClassDTO>> Get()
+        {
+            var licenseClassDTO = clsLicenseClass.GetAllLicenseClasses();
+            if (licenseClassDTO == null)
+            {
+                return NotFound("Not found");
+            }
+            return Ok(licenseClassDTO);
+        }
+
     }
 }
